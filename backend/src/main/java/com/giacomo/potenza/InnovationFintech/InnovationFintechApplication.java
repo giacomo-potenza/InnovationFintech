5c3d0c98b35c234f5f044f8e27b42e8078aa5728
Giacomo Potenza <giacomo.potenza2344@gmail.com>
2025-05-20 19:01:19 +0200

    initial import

---
+++ backend/src/main/java/com/giacomo/potenza/InnovationFintech/InnovationFintechApplication.java
@@ -0,0 +1,13 @@
+package com.giacomo.potenza.InnovationFintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovationFintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovationFintechApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/giacomo/potenza/InnovationFintech/InnovationFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giacomo.potenza.InnovationFintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovationFintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
